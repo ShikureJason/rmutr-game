c73be2f9e9292248677973311114557e820c019a
Shikure <koonjjzaa@gmail.com>
2024-03-12 16:35:00 +0700

    create menu ui

---
+++ Assets/Scripts/Manage/UI/Menu/UIMainMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public void SetActiveMainMenu(bool expression)
+    {
+        gameObject.SetActive(expression);
+    }
+
+    public void StartNewGame()
+    {
+
+    }
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+
+}
